e721bbfa9537a64a80df92a2c9d87697f9a29ea1
unknown <gabimaister@yahoo.ca>
2023-01-22 20:40:27 +0200

    initial

---
+++ JavaApp/src/main/java/com/mycompany/javaapp/OLD/MyApp.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javaapp;
+
+import static com.mycompany.javaapp.abstract_class.show;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.time.Duration;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class MyApp {
+
+    public static void main(String[] args) {
+
+        
+        // ABSTRACT CLASS
+//        abstract_class.main();
+//         
+//        Samsung obj2 = new Samsung();
+//        show(obj2);
+//        
+ try {
+ 
+}
+catch(Exception e) {
+   System.exit (0);
+}
+
+
+        // SELENIUM TEST CLASS TESTING
+        //selenium test = new selenium();
+        //test.eightComponents();
+
+    }
+
+}
+
